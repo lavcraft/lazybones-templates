3a5528655f72fa0061ff3fd0db2bceac641cce7a
aatarasoff <aatarasoff@gmail.com>
2015-10-11 22:10:46 +0300

    new docker-compose

--- templates/ms-base/lazybones.groovy
+++ templates/ms-base/lazybones.groovy
@@ -4,6 +4,7 @@ props.group = ask("Define value for 'group' [ru.joker]: ", "ru.joker", "group")
 props.version = ask("Define value for 'version' [0.0.1]: ", "0.0.1", "version")
 props.dockerHost = ask("Define value for 'docker_host' [192.168.99.100]: ", "192.168.99.100", "dockerHost")
 props.projectName = props.group.split('\\.').last()
+props.dns = "172.17.42.1"
 
 processTemplates "build.gradle", props
 processTemplates "docker-compose.yml", props
