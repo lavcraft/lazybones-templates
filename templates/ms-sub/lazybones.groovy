3433a20920a7bcfd42446553571fd903516b6257
Tolkachev Kirill <lavcraft@gmail.com>
2015-10-11 18:10:11 +0300

    Add Docker support in subprojects and version property

--- templates/ms-sub/lazybones.groovy
+++ templates/ms-sub/lazybones.groovy
@@ -7,12 +7,13 @@ def props = [:]
 
 props.group = ask("Define value for 'group' [ru.joker]: ", "ru.joker", "group")
 props.version = ask("Define value for 'version' [0.0.1]: ", "0.0.1", "version")
-props.dockerHost = ask("Define value for 'docker_host' [192.168.99.100]: ", "192.168.99.100", "dockerHost")
+props.projectName = projectDir.name
 
 String mainApplicationFileName = 'Application.groovy'
 
 processTemplates "build.gradle", props
 processTemplates mainApplicationFileName, props
+processTemplates 'Dockerfile', props
 
 String pkgPath = props.group.replace('.' as char, '/' as char)
 
